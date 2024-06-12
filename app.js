4137862b5b9adde38297741c868a5fbe3673f7d9
Raul <raul010280@gmail.com>
2024-06-12 00:21:52 -0400

    Lo intente

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 // Rock Paper Seasor!
-let rock = "roca";
-let paper = "papel";
-let sisort = "tijera";
+
+
+// Variables
+let Items = ["ROCK", "PAPEL", "TIJERA"];
+
 let numeroRandom = Math.floor(Math.random()* 3);
 
+let computerScore = 0;
+let humanScore = 0
 
+// Funcion de computadora
 let seleccionDeComputadora = function(){
-
+    return Items[numeroRandom];
+}
+// Funcion Humana
+let SeleccionHumana = function(seleccion){
+    return  seleccion = prompt("Select from this: Roca, Papel o Tijera").toUpperCase();
 }
 
-console.log(numeroRandom)
+
+// Game Logic
+
+let playRound = function(humanChoice, ComputerChoice){
+humanChoice = SeleccionHumana;
+ComputerChoice = seleccionDeComputadora;
+
+if(humanChoice == "ROCK" && ComputerChoice == "TIJERA" || humanChoice == "TIJERA" && ComputerChoice == "PAPEL" || humanChoice == "PAPEL" && ComputerChoice == "ROCK"
+){
+    console.log("El Humano ha Ganado")
+}else{
+    console.log("La Computadora ha ganado")
+}
+}
+
+console.log(playRound())
